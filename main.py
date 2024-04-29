dfd139e7fcb7f52e9b26b11db1778a72399b5a23
AvocadoStyle <42403145+AvocadoStyle@users.noreply.github.com>
2024-04-30 00:33:09 +0300

    Removed the hardcoded cache path from the infrastructure of the code and started to use the OO infra

--- main.py
+++ main.py
@@ -6,6 +6,7 @@ if __name__ == "__main__":
     like_counter = 0
     dislike_counter = 0
     check_counter = 1000
+    # represents locations
     saved_locations = ["`Akko", "Qiryat Yam", "Haifa", "Qiryat Motzkin", "Qiryat Bialik", "`Afula", "Yoqne`am",
                        "Or `Aqiva", "Tiberias", "Shelomi", "Karmi’el", "Ma`alot", "Hadar HaKarmel"]
     driver_path = "chromedriver.exe"
@@ -14,36 +15,15 @@ if __name__ == "__main__":
     password = T["password"]
     # ok = OkCupidSelenium(user_name, password, PATH)
     # contain = "*משחק אותה שלא מתלהב בכלל* מה קורה?"
-    contain = "אז מה את עושה בחיים חוץ מלדגמן?"
+    # contain = "*משחק אותה שלא מתלהב* מה קורה?"
+    contain = "את ממש מוכרת לי"
     people = 50000
-    ok = OkCupidSelenium(user_name, password, driver_path, okcupid_url)
+    # gets the cache data path of the user, And if there isn't it will retrieve `None`, And it's valid behavior.
+    usr_cached_data = T.get('user_cache_data')
+    ok = OkCupidSelenium(user_name, password, driver_path, okcupid_url, '')
+    people_max_qty_to_msg = 40
     while True:
-        ok.like_action.like()
-        time.sleep(10)
-        # ok.login(driver)
-        # input("type when you're ready")
-        ######################################################################
-        # method 1: like dislike method
-        # while(check_counter):
-        #     location = ok.location(driver)
-        #     print(location)
-        #     for s in saved_locations:
-        #         if(location == s):
-        #             like_counter += 1
-        #             print("like: {} people\n".format(like_counter))
-        #             ok.like(driver)
-        #             continue
-        #     ok.dislike(driver)
-        #     dislike_counter += 1
-        #     check_counter -= 1
-        # print("the like counter is: {}\n".format(like_counter))
-        # print("the dislike counter is: {}\n".format(dislike_counter))
-        ####################################################################
-
-        # ok.message(driver, contain, people)
-        # classTool.time.sleep(100)
-        # ok.close(driver)
-
-
-
-
+        ok.message(contain=contain, people=people_max_qty_to_msg)
+        # ok.like_action.like_quantity(quantity=10)
+        # people_max_qty_to_msg = 10
+        time.sleep(2)
---
+++ model/action/message_action.py
@@ -0,0 +1,17 @@
+import time
+
+from selenium.common import ElementClickInterceptedException, NoSuchElementException, TimeoutException
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.wait import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+
+
+from model.Utilities.operation_utils import sleep_decorator
+from model.action.browser_action import BrowserAction
+
+
+class Message:
+    def __init__(self, browser_driver):
+        self.browser_driver = browser_driver
+        self.browser_action = BrowserAction(self.browser_driver)
+
